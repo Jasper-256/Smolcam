dc3e01713b8aefb5fdc9d1bc1b0aaf955bf15967
Jasper Morgal <jpmorgal8@gmail.com>
2025-12-29 12:05:40 -0800

    Initial Commit

---
+++ SmolcamTests/SmolcamTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmolcamTests.swift
+//  SmolcamTests
+//
+//  Created by Jasper Morgal on 2025-12-29.
+//
+
+import Testing
+@testable import Smolcam
+
+struct SmolcamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
